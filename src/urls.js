62654775de88dfb6b0c89f2b7b4205edee97933d
Matheus Sampaio <matheus1401@gmail.com>
2015-11-23 02:26:56 -0600

    chore: add login url

--- src/urls.js
+++ src/urls.js
@@ -4,14 +4,15 @@
  * @type {Object}
  */
 
-const base = 'https://www.urionlinejudge.com.br/judge/pt';
+const base = 'https://www.urionlinejudge.com.br';
 
 const URLS = {
     base,
 
-    problemView: base + '/problems/view/',
-    problemSubmit: base + '/runs/add/',
-    problemSubmissions: base + '/runs',
+    login: base + '/judge/login',
+    problemView: base + '/judge/pt/problems/view/',
+    problemSubmit: base + '/judge/pt/runs/add/',
+    problemSubmissions: base + '/judge/pt/runs',
 };
 
 export default URLS;
